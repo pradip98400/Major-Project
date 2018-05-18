913bf09b30a62ef096c13f3f2912a151873256e2
pradip <pradipshrestha718@gmail.com>
2018-05-18 08:35:26 +0545

    Basic Design

--- src/components/Feeds.js
+++ src/components/Feeds.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import './Home.css';
+import './Feeds.css';
 
 export default class Feeds extends Component{
     render(){
@@ -7,13 +7,57 @@ export default class Feeds extends Component{
             <div>
                     <div class="container">
                         <div class="row">
-                        <div class=" layout_3col_left">
-                            Feeds
+                        <div class="col-sm-3">
+                            {/*Feeds*/}
+                            <table class="table table-hover col-sm-4" width='50%'>
+                                {/*Feeds*/}
+                                <thead>
+                                    <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Feeds</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                    <th scope="row">1</th>
+                                    <td>Top Stories</td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">2</th>
+                                    <td>New Questions</td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Bookmarked Answers</td>
+                                    </tr>
+                                </tbody>
+                                {/* Links*/}
+                                <thead>
+                                    <tr>
+                                    <th scope="col"></th>
+                                    <th scope="col">Links</th>
+                                    </tr>
+                                 </thead>
+                                 <tbody>
+                                    <tr>
+                                    <th scope="row">1</th>
+                                    <td>Information technology</td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">2</th>
+                                    <td>Health</td>
+                                    </tr>
+                                    <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Science</td>
+                                    </tr>
+                                 </tbody>
+                                </table>
                         </div>
-                        <div class="col-sm">
+                        <div class="col-sm-7">
                             One of three columns
                         </div>
-                        <div class="col-sm">
+                        <div class="col-sm-2">
                             Improve Your Feed
                         </div>
                     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
 import './Home.css';
+import './Feeds.css';
 {/*import { Navbar, Nav,NavItem, NavDropdown, MenuItem,Jumbotron } from 'react-bootstrap';*/}
 
 
@@ -7,14 +8,14 @@ import './Home.css';
         render(){
             return(
                 <div>
-            
+
                     <nav className="navbar navbar-expand-lg navbar navbar-dark bg-primary">
                         <a className="navbar-brand" href="/home">Notification System</a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                         </button>
-                
+
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
@@ -26,6 +27,7 @@ import './Home.css';
                             <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
+                                <span class="glyphicon glyphicon-bell" aria-hidden="true"></span>
                                 Notifications
                             </a>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
