6ab34ac0fae0c9d8d163b5a8f773ed395d106bd4
lazykingme <puskarstha8@gmail.com>
2018-05-25 20:43:19 +0545

    query ko js file

--- src/components/Query.js
+++ src/components/Query.js
@@ -1,15 +1,41 @@
-import React,{Component} from 'react';
-//styling ko lagi css ne import gardeko chu Query.css
-import "./Query.css";
-export default class Query extends Component{
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './Login.css';
+//import Google from './GoogleLogin'
 
-    render(){
+class query extends Component {
 
-        return(
-            //div le enclosed gar hai, return bhitra yeuta matra tag le enclosed garnu parcha and nested garna pauchas
-            <div>
-                <p className="purple">Oi Puskar Chor yeta kaam gar tero Query ko</p>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      userquery:'',
+    };
+  }
+  onChange = (e) => {
+    const state = this.state
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
+  
+
+  render() {
+    const {userquery } = this.state;
+    return (
+      <div class="container">
+        <form class="form-signin" onSubmit={this.onSubmit}>
+          <h2 class="form-signin-heading" color="blue">Want to ask something? ask here!</h2>
+         
+          <label for="inputQuery" class="sr-only">query</label>
+          <input type="query" class="form-control" placeholder="want to ask something? ask here!" name="userquery" value={userquery} onChange={this.onChange} required/>
+        
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Ask</button>
+          
+        </form>
+      </div>
+    );
+  }
 }
+
+export default query;
