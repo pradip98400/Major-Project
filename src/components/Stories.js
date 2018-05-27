b2bdea644589ff21963340dc1b5363aa4440d83d
pradip <pradipshrestha718@gmail.com>
2018-05-27 18:43:49 +0545

    top stories edited

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,13 +2,41 @@ import React,{Component} from 'react';
 //styling ko lagi css ne import gardeko chu Query.css
 export default class Stories extends Component{
 
-    render(){
-
-        return(
-            //div le enclosed gar hai, return bhitra yeuta matra tag le enclosed garnu parcha and nested garna pauchas
-            <div>
-                <p>Top Stories</p>
-            </div>
-        )
+    constructor() {
+        super();
+        this.state = {
+          userquery:'',
+          queries:[]
+        };
+      }
+      
+    componentDidMount(){
+      fetch('http://localhost:5000/query').
+        then((Response)=>Response.json()).
+        then(data =>{
+          console.log("data is:",data);
+            this.setState({queries:data})
+            console.log(this.state.queries[0].query);
+        })
+        
     }
+      render() {
+        return (
+          <div class="container">
+            {this.state.queries.map(function(item, key) {
+              return(
+            <div>
+    
+            <hr/>
+            <div  className="list-group-item list-group-item-secondary row">
+            {item.query}
+            </div>
+             
+            </div>
+              )
+          })}
+          </div>
+        );
+     
+      }
 }
